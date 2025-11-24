08b009958165ad89c6343bdd8d7f74cff585c603
rossrobino <rbrobino@gmail.com>
2025-11-23 21:51:18 -0500

    export cookie type

--- packages/ovr/src/index.ts
+++ packages/ovr/src/index.ts
@@ -3,3 +3,4 @@ export { Route } from "./route/index.js";
 export { Chunk } from "./chunk/index.js";
 export { jsx, render, type JSX } from "./jsx/index.js";
 export type { Middleware } from "./middleware/index.js";
+export type { Cookie } from "./cookie/index.js";
