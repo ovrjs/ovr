31c5c0a49d85c8e1064fe8d12a5e429af2a52052
Ross Robino <ross@robino.dev>
2026-03-13 11:45:55 -0400

    size

--- packages/size/src/index.ts
+++ packages/size/src/index.ts
@@ -1,11 +1,11 @@
-export const bytes = 26506;
-export const kb = 26.51;
+export const bytes = 26576;
+export const kb = 26.58;
 
-export const App = { bytes: 24466, kb: 24.47 } as const;
+export const App = { bytes: 24493, kb: 24.49 } as const;
 
-export const Field = { bytes: 19359, kb: 19.36 } as const;
+export const Field = { bytes: 19429, kb: 19.43 } as const;
 
-export const Form = { bytes: 17398, kb: 17.4 } as const;
+export const Form = { bytes: 17425, kb: 17.43 } as const;
 
 export const JSX = { bytes: 3529, kb: 3.53 } as const;
 
@@ -13,9 +13,9 @@ export const Multipart = { bytes: 4850, kb: 4.85 } as const;
 
 export const Render = { bytes: 2875, kb: 2.88 } as const;
 
-export const Route = { bytes: 18990, kb: 18.99 } as const;
+export const Route = { bytes: 19017, kb: 19.02 } as const;
 
-export const Schema = { bytes: 17404, kb: 17.4 } as const;
+export const Schema = { bytes: 17431, kb: 17.43 } as const;
 
 export const sizes = {
 	App,
